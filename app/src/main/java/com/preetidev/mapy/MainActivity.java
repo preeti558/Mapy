32be6dab9df79be073f84a5d19b6c75b1fc7e375
preeti558 <pritiverma9354900427@gmail.com>
2023-10-09 20:44:38 +0530

    initial commit

---
+++ app/src/main/java/com/preetidev/mapy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.preetidev.mapy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button showMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showMap=findViewById(R.id.show_map);
+        showMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
